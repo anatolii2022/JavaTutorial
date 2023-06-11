547eca011e4ea07f384d54dd6ef57db4792f9919
WorkstationVMServer <WorkstationVMServer@DESKTOP-4BUC3AP.fritz.box>
2023-06-11 21:26:27 +0200

    new tutorials

--- JavaTutorial/src/Mensch.java
+++ JavaTutorial/src/Mensch.java
@@ -1,31 +1,56 @@
 //JavaTutorial 29
-public class Mensch {
-	int alter;
+public class Mensch extends Lebewesen {
+
 	int iq = 0;
-	String name;
-	String haarfarbe;
-	
+	int hausnummer;
+	int groesse;
+
+	private static int anzahlMenschen = 0;
+
+	static int getAnzahlMenschen() {
+		return anzahlMenschen;
+	}
+
 	void bildung() {
-		iq+=5;
+		iq += 5;
 	}
-	
+
 	void bildung(int wieviel) {
-		iq+=wieviel;
+		iq += wieviel;
 	}
-	
-	//Konstruktor
-	Mensch (int wieAlt, int welcherIQ){
+
+	// Konstruktor
+	Mensch(int wieAlt, int welcherIQ) {
 		alter = wieAlt;
 		iq = welcherIQ;
 	}
-	
-	Mensch (String welcherName, String welcheHaarfarbe){
+
+	Mensch(String welcherName, String welcheHaarfarbe) {
 		welcherName = name;
 		welcheHaarfarbe = haarfarbe;
 	}
+
+	Mensch(int alter, int hausnummer, int groesse) {
+		this.alter = alter;
+		this.hausnummer = hausnummer;
+		this.groesse = groesse;
+	}
+
+	Mensch(int alter, int iq, String name, String haarfarbe) {
+		this.alter = alter;
+		this.iq = iq;
+		this.name = name;
+		this.haarfarbe = haarfarbe;
+		anzahlMenschen++;
+	}
 	
-	//Default Konstruktor
-	Mensch (){}
+	// Default Konstruktor
+	Mensch() {
+	}
 	
+	@Override
+	public String toString() {
+		return String.format("Name: %s\nAlter: %s\nIQ: %s\nHaarfarbe: %s\nSpeicheradresse: %s", name, alter, iq, haarfarbe, super.toString());
+	}
 
 }
